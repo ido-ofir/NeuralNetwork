4a9784911d4fd2f00b825cfb23205c47d4c2dabb
ido-ofir <idoofirs@gmail.com>
2023-06-19 16:21:58 +0300

    initial commit

---
+++ src/AppNetworkOld.jsx
@@ -0,0 +1,164 @@
+import React, { useState, useRef } from 'react'
+import NeuralNetworkCanvas from './NeuralNetworkCanvas'
+
+
+function sigmoid(x) {
+  return 1 / (1 + Math.exp(-x));
+}
+
+function sigmoidDerivative(x) {
+  const fx = sigmoid(x);
+  return fx * (1 - fx);
+}
+
+
+class Neuron {
+  constructor(numOfInputs) {
+    this.weights = Array.from(
+      { length: numOfInputs },
+      () => Math.random() * 2 - 1
+    );
+    this.bias = Math.random() * 2 - 1;
+  }
+
+  feedforward(inputs) {
+    let total = 0;
+    for (let i = 0; i < inputs.length; i++) {
+      total += this.weights[i] * inputs[i];
+    }
+    total += this.bias;
+    return sigmoid(total);
+  }
+}
+
+class MLP {
+  constructor(layers) {
+    this.layers = layers
+    this.hidden = [new Neuron(2), new Neuron(2), new Neuron(2), new Neuron(2)];
+    this.output = new Neuron(4);
+  }
+
+  feedforward(inputs) {
+    let outputs = inputs
+    this.layers[0].map((layer, i) => {
+        layer[1] = outputs[i]
+    })
+    for (let q = 1; q < this.layers.length; q++) {
+      outputs = this.layers[q].map(neuron => this.neuronFeedforward(neuron, outputs))
+    }
+    return outputs;
+  }
+
+  neuronFeedforward(neuron, inputs) {
+    let total = 0;
+    const [weights, bias] = neuron
+    for (let i = 0; i < inputs.length; i++) {
+      total += weights[i] * inputs[i];
+    }
+    total += bias;
+    return sigmoid(total);
+  }
+
+  train(inputs, targets, epochs, lr) {
+    for (let e = 0; e < epochs; e++) {
+      for (let i = 0; i < inputs.length; i++) {
+          const hiddenOut = this.hidden.map((n) => n.feedforward(inputs[i]));
+          const output = this.output.feedforward(hiddenOut);
+          const error = targets[i] - output;
+        const outputDirevative = sigmoidDerivative(output);
+        const outputWeights = this.output.weights;
+
+        for (let j = 0; j < this.hidden.length; j++) {
+          const hiddenDirevative = sigmoidDerivative(hiddenOut[j]);
+          const factor = error * hiddenDirevative * outputDirevative;
+          const hidden = this.hidden[j];
+          for (let k = 0; k < hidden.weights.length; k++) {
+            const inputVal = inputs[i][k];
+            hidden.weights[k] =
+              hidden.weights[k] + factor * outputWeights[j] * inputVal * lr;
+          }
+          hidden.bias = hidden.bias + factor * outputWeights[j] * lr;
+        }
+
+        for (let j = 0; j < outputWeights.length; j++) {
+          outputWeights[j] +=
+            error * sigmoidDerivative(output) * hiddenOut[j] * lr;
+        }
+        this.output.bias += error * sigmoidDerivative(output) * lr;
+      }
+    }
+  }
+}
+
+let inputs = [
+  [0, 0],
+  [0, 1],
+  [1, 0],
+  [1, 1],
+]; // inputs for XOR gate
+let targets = [0, 1, 1, 0]; // targets for XOR gate
+const random = () => Math.random() * 2 - 1
+const neuron = (numWeights) => [Array.from({ length: numWeights }, random), random()]
+const layer = (length, prevLayerLength) => Array.from({ length }, () => neuron(prevLayerLength))
+
+let mlp = new MLP([
+  layer(2, 0),
+  layer(4, 2),
+  layer(1, 4),
+]);
+
+const App = () => {
+  const [result, setResults] = useState(inputs.map(i => mlp.feedforward(i)[0].toFixed(2)))
+  const index = useRef(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const interval = useRef(null)
+  const step = () => {
+    let i = index.current + 1
+    if (i >= inputs.length){
+      i = 0
+    }
+    const input = inputs[i]
+    const target = targets[i]
+    mlp.train([input], [target], 1, 0.2);
+    setResults(inputs.map(i => mlp.feedforward(i)[0].toFixed(2)))
+    index.current = i
+  }
+  const play = () => {
+    setIsPlaying(true)
+    interval.current = setInterval(step, 100)
+  }
+  const stop = () => {
+    setIsPlaying(false)
+    clearInterval(interval.current)
+  }
+  const input = inputs[index.current]
+  const target = targets[index.current]
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%', background: '#888' }}>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: 20 }}>
+        <div style={{ display: 'flex', gap: 20 }}>
+        <NeuralNetworkCanvas network={[input.map(n => ({ bias: n })), mlp.hidden,[mlp.output]]}/>
+        </div>
+        <div>
+          {
+            result.map((r, i) => <div key={i}>{r}</div>)
+          }
+          <div>
+            <button onClick={step}>
+              Step
+            </button>
+            <button onClick={play}>
+              Play
+            </button>
+            <button onClick={stop}>
+              Stop
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/NeuralNetworkCanvas.jsx
@@ -0,0 +1,84 @@
+import React, { useRef, useEffect } from 'react';
+
+function getColor(value) {
+    // Ensure the value is within the expected range
+    value = Math.max(-1, Math.min(1, value));
+  
+    // Map the value from [-1, 1] to [0, 120]
+    const hue = ((value + 1) / 2) * 120;
+  
+    // Return an HSL color string
+    return `hsl(${hue}, 100%, 50%)`;
+  }
+function NeuralNetworkCanvas({ network }) {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+        
+        // Determine size of canvas based on network size
+        const layerCount = network.length;
+        let minWeight = 100;
+        let maxWeight = -100;
+        let minBias = 100;
+        let maxBias = -100;
+        const maxNeuronCount = Math.max(...network.map((layer, i) => {
+            i > 0 && layer.map(t => {
+                minWeight = Math.min(minWeight, ...t.weights);
+                maxWeight = Math.max(maxWeight, ...t.weights);
+                minBias = Math.min(minBias, t.bias);
+                maxBias = Math.max(maxBias, t.bias);
+            })
+            return layer.length
+        }));
+        canvas.height = layerCount * 100; // Space layers 100px apart vertically
+        canvas.width = maxNeuronCount * 100; // Space neurons 100px apart horizontally
+        const weightSpread = maxWeight - minWeight;
+        const biasSpread = maxBias - minBias;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        // Iterate over layers
+        for (let i = 0; i < network.length; i++) {
+            const layer = network[i];
+            const layerNeuronCount = layer.length;
+
+            // Calculate horizontal start position
+            const startHorizontalPos = (canvas.width - (layerNeuronCount * 100)) / 2;
+
+            // Iterate over neurons in layer
+            for (let j = 0; j < layer.length; j++) {
+                const neuron = layer[j];
+                
+                // Draw neuron
+                const bias = (neuron.bias - minBias) / biasSpread * 120
+                context.beginPath();
+                context.arc(startHorizontalPos + j * 100 + 50, i * 100 + 50, 20, 0, 2 * Math.PI, false);
+                context.fillStyle = `hsl(${bias}, 100%, 50%)`;
+                context.fill();
+                
+                // If not the first layer, draw weights
+                if (i > 0) {
+                    const prevLayer = network[i - 1];
+                    const prevLayerNeuronCount = prevLayer.length;
+                    const startPrevLayerPos = (canvas.width - (prevLayerNeuronCount * 100)) / 2;
+
+                    for (let k = 0; k < prevLayer.length; k++) {
+                        const weight = (neuron.weights[k] - minWeight) / weightSpread * 120
+                        context.beginPath();
+                        context.moveTo(startPrevLayerPos + k * 100 + 50, (i - 1) * 100 + 50);
+                        context.lineTo(startHorizontalPos + j * 100 + 50, i * 100 + 50);
+                        context.lineWidth = Math.abs(neuron.weights[k]) * 2;
+                        context.strokeStyle = `hsl(${weight}, 100%, 50%)`;
+                        context.stroke();
+                    }
+                }
+            }
+        }
+    }, [network]);
+
+    return (
+        <canvas ref={canvasRef} />
+    );
+}
+
+export default NeuralNetworkCanvas;
