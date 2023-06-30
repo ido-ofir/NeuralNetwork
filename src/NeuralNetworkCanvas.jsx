ca5b679f6f4ef364364e4285a418a27c95d88360
ido-ofir <idoofirs@gmail.com>
2023-07-01 01:01:35 +0300

    canvas 3

--- src/NeuralNetworkCanvas.jsx
+++ src/NeuralNetworkCanvas.jsx
@@ -1,26 +1,26 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function sigmoid(x) {
     return 1 / (1 + Math.exp(-x));
-  }
+}
 
 const getColor = (value) => {
     // Ensure the value is within the expected range
     value = Math.max(-1, Math.min(1, value));
-  
+
     // Map the value from [-1, 1] to [0, 120]
     const hue = ((value + 1) / 2) * 120;
-  
+
     // Return an HSL color string
     return `hsl(${hue}, 100%, 50%)`;
-  }
+}
 export const NeuralNetworkCanvas = ({ network }) => {
     const canvasRef = useRef(null);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
-        
+
         // Determine size of canvas based on network size
         const layerCount = network.length;
         const maxNeuronCount = Math.max(...network.map((layer, i) => layer.length));
@@ -40,26 +40,28 @@ export const NeuralNetworkCanvas = ({ network }) => {
                 const neuron = layer[j];
 
                 // If not the first layer, draw weights
+                const bias = (sigmoid(neuron.bias) + 1) * 127;
+                const biasRgb = `rgb(${bias}, ${bias}, ${bias})`
+                context.beginPath();
+                context.lineWidth = Math.abs(neuron.output * 10);
+                context.arc(startHorizontalPos + j * 100 + 50, i * 100 + 50, 20, 0, 2 * Math.PI, false);
+                // context.strokeStyle = neuron.output > 0 ? "#0f0" : "#f00";
+                // context.stroke();
+                context.fillStyle = biasRgb;
+                context.fill();
+
                 if (i > 0) {
                     const prevLayer = network[i - 1];
                     const prevLayerNeuronCount = prevLayer.length;
                     const startPrevLayerPos = (canvas.width - (prevLayerNeuronCount * 100)) / 2;
 
                     for (let k = 0; k < prevLayer.length; k++) {
-                        const output = prevLayer[k].output;
-                        context.beginPath();
-                        context.moveTo(startPrevLayerPos + k * 100 + 50, (i - 1) * 100 + 50);
-                        context.lineWidth = Math.abs(neuron.weights[k]) * 4;
-                        context.lineTo(startHorizontalPos + j * 100 + 50, i * 100 + 50);
-                        const strokeStyle = output >= 0 ? `rgba(0, 255, 0, ${output})` : `rgba(255, 0, 0, ${-output})`
-                        context.strokeStyle = strokeStyle;
-                        context.stroke();
 
-                        const weight = (sigmoid(neuron.weights[k]) + 1) * 127;
+                        const weight = sigmoid(neuron.weights[k]) * 255;
                         const weightRgb = `rgb(${weight}, ${weight}, ${weight})`
                         context.beginPath();
                         context.moveTo(startPrevLayerPos + k * 100 + 50, (i - 1) * 100 + 50);
-                        context.lineWidth = Math.abs(neuron.weights[k]) * 2;
+                        context.lineWidth = Math.abs(neuron.weights[k]) * 4;
                         context.lineTo(startHorizontalPos + j * 100 + 50, i * 100 + 50);
                         context.strokeStyle = weightRgb;
                         context.stroke();
@@ -80,15 +82,31 @@ export const NeuralNetworkCanvas = ({ network }) => {
                 const neuron = layer[j];
 
                 // Draw neuron
-                const bias = (sigmoid(neuron.bias) + 1) * 127;
-                const biasRgb = `rgb(${bias}, ${bias}, ${bias})`
+                const outputRgb = `rgba(100, 100, 255, ${neuron.output})`
                 context.beginPath();
-                context.lineWidth = Math.abs(neuron.output * 10);
-                context.arc(startHorizontalPos + j * 100 + 50, i * 100 + 50, 20, 0, 2 * Math.PI, false);
-                context.strokeStyle = neuron.output > 0 ? "#0f0" : "#f00";
-                context.stroke();
-                context.fillStyle = biasRgb;
+                context.lineWidth = Math.abs(neuron.output * 4);
+                context.arc(startHorizontalPos + j * 100 + 50, i * 100 + 50, 14, 0, 2 * Math.PI, false);
+                // context.strokeStyle = neuron.output > 0 ? "#0f0" : "#f00";
+                // context.stroke();
+                context.fillStyle = outputRgb;
                 context.fill();
+
+                if (i > 0) {
+                    const prevLayer = network[i - 1];
+                    const prevLayerNeuronCount = prevLayer.length;
+                    const startPrevLayerPos = (canvas.width - (prevLayerNeuronCount * 100)) / 2;
+
+                    for (let k = 0; k < prevLayer.length; k++) {
+                        const output = sigmoid(neuron.weights[k] * prevLayer[k].output);
+                        context.beginPath();
+                        context.moveTo(startPrevLayerPos + k * 100 + 50, (i - 1) * 100 + 50);
+                        context.lineWidth = Math.abs(neuron.weights[k]) * 2;
+                        context.lineTo(startHorizontalPos + j * 100 + 50, i * 100 + 50);
+                        const strokeStyle = `rgba(100, 100, 255, ${output})`
+                        context.strokeStyle = strokeStyle;
+                        context.stroke();
+                    }
+                }
             }
         }
     }, [network]);
